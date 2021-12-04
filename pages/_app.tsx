9d9e8b0850940b9c507ce81297203cb69aed5778
Emilio Schepis <emilio.schepis@gmail.com>
2021-12-04 11:49:14 +0100

    feat(ui): add chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import "../styles/globals.css";
 
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
+import theme from "../src/theme/theme";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
